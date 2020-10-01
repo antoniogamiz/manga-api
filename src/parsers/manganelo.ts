8b54a6a1f730242d487c10f3c5f739aaa03983d6
antoniogamiz <antoniogamiz10@gmail.com>
2020-10-01 13:30:44 +0200

    feat: declare main parser (#6)
    I have set random values as returns in order to not break the tests

---
+++ src/parsers/manganelo.ts
@@ -0,0 +1,37 @@
+import { MangaParser, Manga, Genre, Status, Chapter } from "../types";
+
+/**
+ * Parse for the [Manganelo](https://manganelo.com/) manga site.
+ */
+class ManganeloParser implements MangaParser {
+  parse(html: string): Manga {
+    return {
+      title: "",
+      alternativeTitles: [],
+      status: Status.COMPLETED,
+      genres: [],
+      chapters: [],
+    };
+  }
+  parseTitle(html: string): string {
+    return "";
+  }
+  parseAlternativeTitles(html: string): string[] {
+    return [];
+  }
+  parseStatus(html: string): Status {
+    return Status.COMPLETED;
+  }
+  parseGenres(html: string): Genre[] {
+    return [];
+  }
+  parseChapters(html: string): Chapter[] {
+    return [];
+  }
+  parseChapter(html: string): Chapter {
+    return {
+      title: "",
+      chapterPages: [],
+    };
+  }
+}
