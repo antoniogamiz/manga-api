d30d8847ac42454a5ab25577747a5ad481d16d8f
antoniogamiz <antoniogamiz10@gmail.com>
2020-10-06 20:19:47 +0200

    feat: add POST /manga endpoint (#24)

--- src/routes/manga.ts
+++ src/routes/manga.ts
@@ -1,9 +1,32 @@
 import { Router, Request, Response } from "express";
-
+import { MangaModel } from "../models";
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
   return res.send({ titles: [] });
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  const {
+    title,
+    alternativeTitles,
+    status,
+    genres,
+    chapters,
+    chaptersEntries,
+  } = req.body;
+
+  const manga = MangaModel.build({
+    title,
+    alternativeTitles,
+    status,
+    genres,
+    chapters,
+    chaptersEntries,
+  });
+
+  await manga.save();
+  return res.status(201).send(manga);
 });
 
 export { router as mangaRouter };
