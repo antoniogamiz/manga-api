74f2f48acf5bd924e8259c147982f101d10d342d
antoniogamiz <antoniogamiz10@gmail.com>
2020-10-06 10:15:45 +0200

    chore: add body-parser (#24)

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import dotenv from "dotenv";
 import express from "express";
+import * as bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = process.env.SERVER_PORT;
 
 app.get("/", (req, res) => {
