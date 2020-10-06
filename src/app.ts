0db29abc3a03db4378be16e0e92d2445496735c5
antoniogamiz <antoniogamiz10@gmail.com>
2020-10-06 12:27:40 +0200

    feat: connect mongoDB, close #25

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import express, { Response, Request } from "express";
 import * as bodyParser from "body-parser";
 import { mangaRouter } from "./routes/manga";
+import mongoose from "mongoose";
+import { exit } from "process";
 
 dotenv.config();
 
@@ -15,6 +17,27 @@ app.use("/manga", mangaRouter);
 app.get("/health", (req: Request, res: Response) =>
   res.send({ uptime: process.uptime(), message: "OK", timestamp: Date.now() })
 );
+
+console.log(process.env.DB_URL);
+mongoose.connect(
+  process.env.DB_URL || "",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("MongoDB connected");
+    }
+  }
+);
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
