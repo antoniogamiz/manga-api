893bc4a4b782b95cd016dd5160d2df5884f3610d
antoniogamiz <antoniogamiz10@gmail.com>
2020-10-06 17:55:33 +0200

    feat: add manga mongoose model, close #26
    Based on: https://medium.com/swlh/typescript-with-mongoose-and-node-express-24073d51d2ee

---
+++ src/models/manga.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import { Manga } from "../types";
+
+interface MangaDocument extends mongoose.Document, Manga {}
+
+interface MangaModelInterface extends mongoose.Model<MangaDocument> {
+  build(attr: Manga): MangaDocument;
+}
+
+const mangaSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  alternativeTitles: {
+    type: [String],
+    required: true,
+  },
+  status: {
+    type: [String],
+    required: true,
+  },
+  genres: {
+    type: [String],
+    required: true,
+  },
+  chapters: {
+    type: [{ title: String, chapterPages: [{ url: String }] }],
+  },
+  chaptersEntries: {
+    type: [{ url: String }],
+    required: true,
+  },
+});
+
+mangaSchema.statics.build = (attr: Manga) => {
+  return new MangaModel(attr);
+};
+
+const MangaModel = mongoose.model<MangaDocument, MangaModelInterface>(
+  "Manga",
+  mangaSchema
+);
+
+export { MangaModel };
