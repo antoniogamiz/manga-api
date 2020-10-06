996662b915091fe3779a8fae4550d8b888c8322a
antoniogamiz <antoniogamiz10@gmail.com>
2020-10-06 07:25:41 +0200

    test: format the test (#20)

--- tests/parsers/manganelo.test.ts
+++ tests/parsers/manganelo.test.ts
@@ -4,12 +4,20 @@ import { Chapter } from "../../src/types/manga";
 
 import test_case from "./chapter_test_cases.json";
 
-test("Get chapter title", async () => {
-  const html: string = await fetchData(test_case.chapters[0].url);
+describe("Parse Chapter", () => {
   const parser = new ManganeloParser();
-  const chapter: Chapter = parser.parseChapter(html);
-  expect(chapter.title).toBe(test_case.chapters[0].title);
-  expect(chapter.chapterPages).toStrictEqual(
-    test_case.chapters[0].chapterPages
-  );
+
+  test("Get chapter title", async () => {
+    const html: string = await fetchData(test_case.chapters[0].url);
+    const chapter: Chapter = parser.parseChapter(html);
+    expect(chapter.title).toBe(test_case.chapters[0].title);
+  });
+
+  test("Get chapter pages", async () => {
+    const html: string = await fetchData(test_case.chapters[0].url);
+    const chapter: Chapter = parser.parseChapter(html);
+    expect(chapter.chapterPages).toStrictEqual(
+      test_case.chapters[0].chapterPages
+    );
+  });
 });
