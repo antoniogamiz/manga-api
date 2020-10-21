e92dbf83d112ddd4155d9916ef4fc646e4b26d52
antoniogamiz <antoniogamiz10@gmail.com>
2020-10-21 23:14:10 +0200

    feat: improve app listen event

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import app from "./src/app.ts";
 
 const port = 8000;
-console.log(`Server listening at http://localhost:${port}`);
 await app.listen({ port });
--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,10 @@ const app = new Application<Context>();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
+app.addEventListener("listen", ({ secure, hostname, port }) => {
+  const protocol = secure ? "https://" : "http://";
+  const url = `${protocol}${hostname ?? "localhost"}:${port}`;
+  console.log(`Listening on: ${port}`);
+});
+
 export default app;
