7ca73303bb6718cf1165903a30202b522fcd2739
antoniogamiz <antoniogamiz10@gmail.com>
2020-11-03 17:22:47 +0100

    feat: implement two new endpoints (#11)

--- src/routes/manga.routes.ts
+++ src/routes/manga.routes.ts
@@ -2,6 +2,7 @@
 import { Response } from "../deps.ts";
 import fetchPage from "../utils/http.ts";
 import { ManganeloParser } from "../parsers/index.ts";
+import { Genre } from "../types/index.ts";
 
 export const getManga = async ({
   params,
@@ -48,6 +49,53 @@ export const getChapter = async ({
   const parser = new ManganeloParser();
   response.status = 200;
   const { data, error } = parser.parseChapter(html);
+  if (error) response.body = { error: error };
+  else response.body = data;
+  return;
+};
+
+export const getMangaByGenre = async ({
+  params,
+  response,
+}: {
+  params: { genre: Genre };
+  response: Response;
+}) => {
+  const parser = new ManganeloParser();
+  const url = parser.genreURLs[params.genre];
+  if (!url) {
+    response.body = { error: "Bad genre used." };
+    return;
+  }
+  const html = await fetchPage(url);
+  response.status = 200;
+  const { data, error } = parser.parseMangaList(html);
+  if (error) response.body = { error: error };
+  else response.body = data;
+  return;
+};
+
+export const getMangaByStatus = async ({
+  params,
+  response,
+}: {
+  params: { status: string };
+  response: Response;
+}) => {
+  if (params.status !== "ongoing" && params.status !== "completed") {
+    response.body = {
+      error: "Bad status: only ongoing or completed are valid.",
+    };
+    return;
+  }
+
+  const html = await fetchPage(
+    `https://manganelo.com/genre-all?state=${params.status}`
+  );
+
+  const parser = new ManganeloParser();
+  response.status = 200;
+  const { data, error } = parser.parseMangaList(html);
   if (error) response.body = { error: error };
   else response.body = data;
   return;
