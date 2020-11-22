80cd17c42d17e35edd81ec52a091de1314e8f612
antoniogamiz <antoniogamiz10@gmail.com>
2020-11-22 19:24:23 +0100

    feat: finish telegram bot, close #75

--- functions/bot.js
+++ functions/bot.js
@@ -1,20 +1,39 @@
-const TelegramBot = require("node-telegram-bot-api");
-const superagent = require("superagent");
+const Telegraf = require("telegraf");
+const fetch = require("node-fetch");
 
-const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const API_URL = process.env.API_URL;
-const bot = new TelegramBot(TOKEN);
 
-bot.setWebHook(
-  `https://upbeat-archimedes-878dc0.netlify.app/.netlify/functions/bot/bot${TOKEN}`
-);
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-exports.handler = async (context, cb) => {
-  //   const mangaID = context.url.match(/\/manga (.+)/)[1];
-  console.log(new Date());
-  console.log(context);
-  //   const mangaID = "lf924506";
-  //   const response = await superagent.get(API_URL + `/mangas/${mangaID}`);
-  //   bot.sendMessage(chatID, "Received");
-  return { statusCode: 200, body: "" };
+bot.start((ctx) => ctx.reply(process.uptime()));
+
+bot.hears(/\/manga (.+)/, async (ctx) => {
+  const mangaID = ctx.message.text.substr(7);
+  const res = await (await fetch(API_URL + `/mangas/${mangaID}`)).json();
+  bot.telegram.sendMessage(ctx.chat.id, res);
+});
+
+bot.hears(/\/available (.+)/, async (ctx) => {
+  const n = ctx.message.text.substr(11);
+  const res = await (await fetch(API_URL + `/available/${n}`)).json();
+  bot.telegram.sendMessage(ctx.chat.id, res);
+});
+
+bot.hears(/\/genre (.+)/, async (ctx) => {
+  const genre = ctx.message.text.substr(7);
+  const res = await (await fetch(API_URL + `/available/genre/${genre}`)).json();
+  bot.telegram.sendMessage(ctx.chat.id, res);
+});
+
+bot.hears(/\/status (.+)/, async (ctx) => {
+  const status = ctx.message.text.substr(8);
+  const res = await (
+    await fetch(API_URL + `/available/status/${status}`)
+  ).json();
+  bot.telegram.sendMessage(ctx.chat.id, res);
+});
+
+exports.handler = async (event, ctx, callback) => {
+  await bot.handleUpdate(JSON.parse(event.body));
+  return callback(null, { statusCode: 200 });
 };
